360f9900baa808d654bce9ae2d8b829e7e7688e5
javad hashemzadeh <javadhashemzadeh78@gmail.com>
2024-01-08 10:49:44 +0330

    edit routes

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
         <Header />
         <Routes>
           <Route index element={<Watchlist />} />
-          <Route path="/react-movies-watchlist/add" element={<Add />} />
-          <Route path="/react-movies-watchlist/watched" element={<Watched />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/watched" element={<Watched />} />
         </Routes>
       </Router>
     </GlobalProvider>
